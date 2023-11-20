80cebd246b1615fdb102a6eac6986b0afbfd8ee9
48cristi4848 <4872910-48cristi4848@users.noreply.replit.com>
2023-11-20 19:28:42 +0000

    Initial commit

---
+++ data/seo.js
@@ -0,0 +1,31 @@
+module.exports = {
+	// GENERAL SEO
+	website: {
+	  title: "BetaDv™",
+		description: "Hello! I'm BetaDv, welcome to my personal website. You can find the majority of my socials on here!",
+		favicon: "assets/images/favicon.ico",
+			},
+
+	// DISCORD / TWITTER EMBEDS
+	embed: {
+		color: "#9375ab",
+		image: "/assets/images/background_large.jpg"
+	},
+
+	// FOR WHEN YOU SEARCH ON BROWSERS
+	searchEntry: {
+		"@context": "http://schema.org",
+		"@type": "Person",
+		name: "BetaDv",
+		image: "/images/image01.jpg",
+		description: "I'm BetaDv, however feel free to call me Beta! Feel free to connect with me on social media!",
+		url: "https://betadv.lag.tf/",
+		sameAs: [
+			"https://www.instagram.com/betadv_/",
+			"https://www.tiktok.com/@betadv_",
+			"https://youtube.com/@betadv",
+			"https://steamcommunity.com/id/BetaDv/",
+			"https://github.com/BetaDv/",
+		]
+	}
+}
